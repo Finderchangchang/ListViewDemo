306e5ce7f0f148fd116e618333ce7abda69a5238
liuweijie <1031066280@qq.com>
2015-12-30 23:39:29 +0800

    123123

--- app/src/main/java/liuliu/demo/list/base/BaseFragment.java
+++ app/src/main/java/liuliu/demo/list/base/BaseFragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import net.tsz.afinal.FinalActivity;
+
 /**
  * Created by Administrator on 2015/11/25.
  */
@@ -23,14 +25,14 @@ public abstract class BaseFragment extends Fragment {
         if (parent != null) {
             parent.removeView(viewRoot);
         }
-//        FinalActivity.initInjectedView(this, viewRoot);
-        initEvents(viewRoot);
+        FinalActivity.initInjectedView(this, viewRoot);
+        initEvents();
         return viewRoot;
     }
 
     public abstract void initViews();
 
-    public abstract void initEvents(View view);
+    public abstract void initEvents();
 
     public void setContentView(int layoutId) {
         mLayId = layoutId;
